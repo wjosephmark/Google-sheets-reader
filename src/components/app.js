39f425d4efd5a2facf008945522a11be04f8ec2a
wjosephmark <wjosephmark@gmail.com>
2020-08-28 20:50:42 -0600

    data now populates into array

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 export default function App() {
 
+const sheetData = []
 
 const GSheetReader = require('g-sheets-api')
 
@@ -9,25 +10,18 @@ const GSheetReader = require('g-sheets-api')
       sheetId: '1wGfiouhuaA-0oB8tMXZgkKKYPmpg1aktwxt1e_hHcG4',
       sheetNumber: 1,
       returnAllResults: true,
-      // filter: {
-      //   'department': 'archaeology',
-      //   'module description': 'introduction'
-      // },
-      // filterOptions: {
-      //   operator: 'or',
-      //   matching: 'loose'
-      // }
     }
 
   GSheetReader(options, results => {
-      
+      console.log(results)
+      sheetData.push(results)
   });
 
   return (
     <div className='app'>
       <h1>DevCamp React Starter</h1>
       <h2>React Redux Router</h2>
-      {/* <button onClick={() => GSheetReader}>Click me</button> */}
+      <button onClick={() => console.log(sheetData)}>Click me</button>
     </div>
   );
 }
